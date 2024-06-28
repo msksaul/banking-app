8a5ec5213645998d7ee2aefe4fc10a765694dea2
mscalel <msksaul@gmail.com>
2024-06-28 14:34:37 -0600

    Transaction History

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,6 +6,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import Footer from './Footer'
+import PlaidLink from './PlaidLink'
 
 
 const Sidebar = ({ user }: SiderbarProps) => {
@@ -43,6 +44,8 @@ const Sidebar = ({ user }: SiderbarProps) => {
             </Link>
           )
         })}
+
+        <PlaidLink user={user}/>
       </nav>
 
       <Footer user={user}/>
